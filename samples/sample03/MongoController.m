22f21dd569b9dc1fd7298a81f9205ea7dc0377a0
Aldrin Martoq <aldrin@martoq.cl>
2014-06-14 20:28:06 -0400

    Create now outputs JSON strings, added ListJSON method, added Date.

--- samples/sample03/MongoController.m
+++ samples/sample03/MongoController.m
@@ -9,6 +9,15 @@
 #import "MongoController.h"
 #import "ObjCMongoDB.h"
 
+
+@implementation NSDate (StringValue)
+
+- (NSString *) stringValue {
+    return [self descriptionWithCalendarFormat:@"%Y-%m-%d %H:%M:%S" timeZone:nil locale:nil];
+}
+
+@end
+
 @implementation MongoController
 
 - (id)list {
@@ -34,15 +43,47 @@
 
 - (id)create {
     unsigned long long random = 0;
-    arc4random_buf(&random, sizeof(random));
     MongoDBCollection *collection = [MongoController collection];
+    NSMutableArray *results = [NSMutableArray array];
     for (int i = 1; i <=10; i++) {
+        NSDate *date = [NSDate date];
+        
+        [date stringValue];
+        
+        arc4random_buf(&random, sizeof(random));
         NSError *error = nil;
-        NSDictionary *data = @{@"codigo" : [NSNumber numberWithUnsignedLongLong:random] , @"nombre" : [NSString stringWithFormat:@"dato %llu", i + random]};
+        NSMutableDictionary *data = [@{@"codigo" : [NSNumber numberWithUnsignedLongLong:random] , @"nombre" : [NSString stringWithFormat:@"dato %llu", i + random], @"cuando" : [NSDate date]} mutableCopy];
+        [results addObject:data];
         [collection insertDictionary:data writeConcern:nil error:&error];
+        [data setObject:[[data objectForKey:@"cuando"] stringValue] forKey:@"cuando"];
     }
     
-    return [NSString stringWithFormat:@"OK %llu", random];
+    NSDictionary *result = @{@"results" : results, @"cuenta" : [NSNumber numberWithUnsignedInteger:[results count]]};
+    
+    NSData *data = [NSJSONSerialization dataWithJSONObject:result options:NSJSONWritingPrettyPrinted error:nil];
+    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+}
+
+- (id)listJSON {
+    unsigned long long random = 0;
+    arc4random_buf(&random, sizeof(random));
+    NSError *error = nil;
+    MongoDBCollection *collection = [MongoController collection];
+    MongoKeyedPredicate *predicate = [MongoKeyedPredicate predicate];
+    [predicate keyPath:@"codigo" isGreaterThan:[NSNumber numberWithUnsignedLongLong:random]];
+    MongoFindRequest *findRequest = [MongoFindRequest findRequestWithPredicate:predicate];
+    findRequest.limitResults = 10;
+    NSMutableArray *results = [[collection findWithRequest:findRequest error:&error] mutableCopy];
+    for (NSUInteger i = 0; i < [results count]; i++) {
+        NSMutableDictionary *d = [[BSONDecoder decodeDictionaryWithDocument:[results objectAtIndex:i]] mutableCopy];
+        [d setObject:[[d objectForKey:@"_id"] stringValue] forKey:@"_id"];
+        [results setObject:d atIndexedSubscript:i];
+    }
+    
+    NSDictionary *result = @{@"results" : results, @"cuenta" : [NSNumber numberWithUnsignedInteger:[results count]]};
+    
+    NSData *data = [NSJSONSerialization dataWithJSONObject:result options:NSJSONWritingPrettyPrinted error:nil];
+    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
 }
 
 + (MongoDBCollection *) collection {
